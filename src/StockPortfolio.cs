308e9133113539e6139e9b21b0d3808499d874ae
timmson <timmson666@mail.ru>
2021-02-04 11:47:48 +0300

    First version

---
+++ src/StockPortfolio.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace coding_dojo_dotnetcore.src
+{
+    public class StockPortfolio
+    {
+        readonly IDictionary<string, int> stocks = new Dictionary<string, int>();
+
+        public StockPortfolio()
+        {
+        }
+
+        public void Add(string stockName, int stockCount)
+        {
+            stocks[stockName] = (stocks.ContainsKey(stockName) ? stocks[stockName] : 0) + stockCount;
+        }
+
+        public int GetSize()
+        {
+            return stocks.Count;
+        }
+
+        public int GetStockCount(string stockName)
+        {
+            return stocks[stockName];
+        }
+
+        public bool IsEmpty()
+        {
+            return GetSize() == 0;
+        }
+    }
+}
---
+++ tests/StockPortfolioShould.cs
@@ -0,0 +1,58 @@
+﻿using coding_dojo_dotnetcore.src;
+using NUnit.Framework;
+
+namespace coding_dojo_dotnetcore.tests
+{
+    public class StockPortfolioShould
+    {
+
+        StockPortfolio stockPortfolio;
+
+        [SetUp]
+        public void SetUp()
+        {
+            stockPortfolio = new StockPortfolio();
+        }
+
+        [Test]
+        public void ReturnBeEmptyAtStart()
+        {
+            Assert.IsTrue(stockPortfolio.IsEmpty());
+        }
+
+
+        [Test]
+        public void ReturnZeroAsSizeOfPortfolioAtStart()
+        {
+            Assert.AreEqual(0, stockPortfolio.GetSize());
+        }
+
+        [Test]
+        public void GiveAbilityToAddStock()
+        {
+            stockPortfolio.Add("APPL", 1);
+            Assert.IsFalse(stockPortfolio.IsEmpty());
+            Assert.AreEqual(1, stockPortfolio.GetSize());
+        }
+
+        [Test]
+        public void GiveAbilityToCountOfUniqueStock()
+        {
+            stockPortfolio.Add("APPL", 1);
+            Assert.IsFalse(stockPortfolio.IsEmpty());
+            Assert.AreEqual(1, stockPortfolio.GetStockCount("APPL"));
+        }
+
+        [Test]
+        public void GiveAbilityToAddMoreThenOneStock()
+        {
+            stockPortfolio.Add("APPL", 1);
+            stockPortfolio.Add("APPL", 1);
+            Assert.IsFalse(stockPortfolio.IsEmpty());
+            Assert.AreEqual(1, stockPortfolio.GetSize());
+            Assert.AreEqual(2, stockPortfolio.GetStockCount("APPL"));
+        }
+
+    }
+
+}
