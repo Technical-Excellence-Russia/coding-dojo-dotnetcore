3d5ee3247cc95d5c43a99198a9ac66821690edb7
timmson <timmson666@mail.ru>
2021-02-05 10:03:32 +0300

    Initial commit

---
+++ src/StringCalculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace coding_dojo_dotnetcore.tests
+{
+    public class StringCalculator
+    {
+        public int Compute(params string[] input)
+        {
+            if (input == null || input.Length == 0 || (input.Length == 1 && input[0] == ""))
+            {
+                return 0;
+            }
+
+            int sum = 0;
+            Array.ForEach(Array.ConvertAll(String.Join(",", input).Split(","), n => Int32.Parse(n)), n => sum+= n);
+            return sum;
+        }
+    }
+}
---
+++ tests/StringCalculatorShould.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+
+namespace coding_dojo_dotnetcore.tests
+{
+    class StringCalculatorShould
+    {
+        readonly StringCalculator stringCalculator = new StringCalculator();
+
+        [Test]
+        public void ReturnZeroWhenInputIsNull()
+        {
+            Assert.AreEqual(0, stringCalculator.Compute(null));
+        }
+
+        [Test]
+        public void ReturnZeroWhenInputIsEmptyString()
+        {
+            Assert.AreEqual(0, stringCalculator.Compute(""));
+        }
+
+        [Test]
+        public void Return1WhenInputIs1()
+        {
+            Assert.AreEqual(1, stringCalculator.Compute("1"));
+        }
+
+        [Test]
+        public void Return2WhenInputIs1And1()
+        {
+            Assert.AreEqual(2, stringCalculator.Compute("1,1"));
+        }
+
+        [Test]
+        public void Return2WhenInputIs1And1With2Arguments()
+        {
+            Assert.AreEqual(2, stringCalculator.Compute("1", "1"));
+        }
+
+        [Test]
+        public void Return6WhenInputIs11And1With2Arguments()
+        {
+            Assert.AreEqual(6, stringCalculator.Compute("1,2", "3"));
+        }
+    }
+}
